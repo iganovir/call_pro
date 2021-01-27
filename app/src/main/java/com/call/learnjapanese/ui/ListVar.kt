1c288db1361974c8845b529ffa175e622954964d
Bagas Marsya <bagas.pratama@wgs.co.id>
2021-01-27 11:35:01 +0700

    List audio

--- app/src/main/java/com/call/learnjapanese/ui/ListVar.kt
+++ app/src/main/java/com/call/learnjapanese/ui/ListVar.kt
@@ -7,8 +7,25 @@ import com.call.learnjapanese.R
  */
 class ListVar {
 
-    /** ini adalah contoh */
+    /** Makanan */
     private val audioMakananJp = mutableListOf(R.raw.makanan_nasi_jp, R.raw.makanan_roti_jp, R.raw.makanan_daging_jp, R.raw.makanan_ikan_jp, R.raw.makanan_telur_jp, R.raw.makanan_sayur_jp, R.raw.makanan_buah_jp )
     private val audioMakananId = mutableListOf(R.raw.makanan_nasi_id, R.raw.makanan_roti_id, R.raw.makanan_daging_id, R.raw.makanan_ikan_id, R.raw.makanan_telur_id, R.raw.makanan_sayur_id, R.raw.makanan_buah_id )
+    private val audioMakananPercakapan = mutableListOf(R.raw.makanan_percakapan_1, R.raw.makanan_percakapan_2, R.raw.makanan_percakapan_3, R.raw.makanan_pola_kalimat)
+    private val audioJamMakanJp = mutableListOf(R.raw.makan_pagi_jp, R.raw.makan_siang_jp, R.raw.makan_malam_jp)
+    private val audioJamMakanId = mutableListOf(R.raw.makan_pagi_id, R.raw.makan_siang_id, R.raw.makan_malam_id)
+
+    /** Minuman Kurang audio Kopi & air jp*/
+    private val audioMinumanJp = mutableListOf(R.raw.minuman_juice_jp, R.raw.minuman_susu_jp, R.raw.minuman_teh_jp, R.raw.minuman_judul, R.raw.minuman_catatan)
+    private val audioMinumanId = mutableListOf(R.raw.minuman_juice_id, R.raw.minuman_susu_id, R.raw.minuman_teh_id, R.raw.minuman_air_id, R.raw.minuman_kopi_id)
+
+    /** Kegiatan */
+    private val audioKegiatanJp = mutableListOf(R.raw.kegiatan_bangun_jp, R.raw.kegiatan_berdoa_jp, R.raw.kegiatan_bersih_bersih_jp, R.raw.kegiatan_datang_ke_sekolah_jp, R.raw.kegiatan_judul, R.raw.kegiatan_mandi_jp, R.raw.kegiatan_membaca_koran_jp, R.raw.kegiatan_mencuci_jp, R.raw.kegiatan_mencuci_muka_jp, R.raw.kegiatan_mendengar_lagu_jp, R.raw.kegiatan_mengerjakan_pr_jp, R.raw.kegiatan_menulis_surat_jp, R.raw.kegiatan_minum_teh_jp, R.raw.kegiatan_nonton_tv_jp )
+    private val audioKegiatanId = mutableListOf(R.raw.kegiatan_bangun_id, R.raw.kegiatan_berdoa_id, R.raw.kegiatan_bersih_bersih_id, R.raw.kegiatan_datang_ke_sekolah_id, R.raw.kegiatan_judul, R.raw.kegiatan_mandi_id, R.raw.kegiatan_membaca_koran_id, R.raw.kegiatan_mencuci_id, R.raw.kegiatan_mencuci_muka_id, R.raw.kegiatan_mendengar_lagu_id, R.raw.kegiatan_mengerjakan_pr_id, R.raw.kegiatan_menulis_surat_id, R.raw.kegiatan_minum_teh_id, R.raw.kegiatan_nonton_tv_id )
+    private val audioKegiatanPercakapan = mutableListOf(R.raw.kegiatan_percakapan_1, R.raw.kegiatan_percakapan_2, R.raw.kegiatan_percakapan_3, R.raw.kegiatan_percakapan_4, R.raw.kegiatan_pola_kalimat)
+
+    /** Kendaraan */
+    private val audioKendaraanJp = mutableListOf(R.raw.kendaraan_bus_jp, R.raw.kendaraan_mobil_jp, R.raw.kendaraan_motor_jp, R.raw.kendaraan_sepeda_jp, R.raw.kendaraan_judul)
+    private val audioKendaraanId = mutableListOf(R.raw.kendaraan_bus_id, R.raw.kendaraan_mobil_id, R.raw.kendaraan_motor_id, R.raw.kendaraan_sepeda_id)
+    private val audioKendaraanPercakapan = mutableListOf(R.raw.kendaraan_percakapan_1, R.raw.kendaraan_percakapan_2, R.raw.kendaraan_percakapan_3, R.raw.kendaraan_percakapan_4, R.raw.kendaraan_pola_kalimat_1, R.raw.kendaraan_pola_kalimat_2)
 
 }
